896c0fedcaf175d0dbd20a455206a2d066904f4e
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2017-06-03 19:49:17 -0300

    Modified relAngle.m file to implement new serial comm format.

--- relAngle.m
+++ relAngle.m
@@ -1,4 +1,4 @@
-function newRobot = relAngle(robot, theta0, theta1, omega0 = 10, omega1 = 10, updateflag = True, comm = True)
+function newRobot = relAngle(robot, theta0, theta1, omega0 = 10, omega1 = 10, updateflag = true, comm = true)
 
 	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 	% robot: the list containing the robot current status and serial address
@@ -14,27 +14,27 @@ function newRobot = relAngle(robot, theta0, theta1, omega0 = 10, omega1 = 10, up
 	% Code to validate the command before sending it to the controller board.
 	% This is to ensure that no dangerous commands are sent to the robot.
 
-	newTheta0 = robot[2] + theta0;
-	newTheta1 = robot[3] + theta1;
+	newTheta0 = robot(2) + theta0;
+	newTheta1 = robot(3) + theta1;
 	newX = 160 + 80*cos(newTheta0) + 80*cos(newTheta0 + newTheta1);
 	newY = 80*sin(newTheta0) + 80*sin(newTheta0 + newTheta1);
-	newRobot = robot
+	newRobot = robot;
 
-	validCommand = True;
+	validCommand = true;
 
 	if(newTheta0 < 0 || newTheta0 > 180)
 		disp('ERROR: Destination angle for first motor out of limits.')
-		validCommand = False;
+		validCommand = false;
 	endif
 
 	if(newTheta1 < -180 || newTheta1 > 180)
 		disp('ERROR: Destination angle for second motor out of limits.')
-		validCommand = False;
+		validCommand = false;
 	endif
 
 	if(omega0 < 0 || omega1 < 0)
 		disp('ERROR: No negative speed values are allowed.')
-		validCommand = False;
+		validCommand = false;
 	endif
 
 	% Code to check the direction of the movement
@@ -44,12 +44,14 @@ function newRobot = relAngle(robot, theta0, theta1, omega0 = 10, omega1 = 10, up
 			theta0 = abs(theta0);
 		else
 			dir0 = 0;
+		endif
 
 		if(theta1 < 0)
 			neg1 = 1;
 			theta1 = abs(theta1);
 		else
 			dir1 = 0;
+		endif
 
 		% Code to calculate how many steps should the motor make
 
@@ -58,38 +60,47 @@ function newRobot = relAngle(robot, theta0, theta1, omega0 = 10, omega1 = 10, up
 
 		% Code to calculate how many microseconds should the motor wait between each step
 
-		delay0 = round(175781.25/omega0);
-		delay1 = round(175781.25/omega1);
+		if(omega0 ~= 0)
+			delay0 = round(175781.25/omega0);
+		else
+			delay0 = 0;
+		endif
+		
+		if(omega1 ~= 0)
+			delay1 = round(175781.25/omega1);
+		else
+			delay1 = 0;
+		endif
 
 		% Code to update the robot current coordinates
 
 		if(updateflag)
-			newRobot = [robot[1], newTheta0, newTheta1, newX, newY];
+			newRobot = [robot(1), newTheta0, newTheta1, newX, newY];
 		endif
 		
 		% Code to format the command message
 
-		message = 'd' + num2str(dir0) + 's' + num2str(steps0) + 't' + num2str(delay0) + 'd' + num2str(dir1) + 's' + num2str(steps1) + 't' + num2str(delay1);
+		message = strcat(num2str(dir0, '%01d'), num2str(steps0, '%04d'), num2str(delay0, '%06d'), num2str(dir1, '%01d'), num2str(steps1, '%04d'), num2str(delay1, '%06d'));
 
 		% Code to send the message
 
 		if(comm)
 			if(isOctave())
-				srl_write(robot[1], message);
+				srl_write(robot(1), message);
 			else
-				fprintf(robot[1], message)
+				fprintf(robot(1), message)
+			endif
+
+			% Code to wait for the controller board response
+
+			begin = time();
+			data = srl_read(robot(1), 1);
+
+			while(data ~= '\n' || time() - begin < 20)
+				data = srl_read(robot(1), 1);
+			endwhile
 		else
 			disp(message)
-			
 		endif
-
-		% Code to wait for the controller board response
-
-		begin = time();
-		data = srl_read(robot[1], 1);
-
-		while(data ~= '\n' || time() - begin < 20)
-			data = srl_read(robot[1], 1);
-		endwhile
 	endif
-end
+endfunction
