fff0e3af517055ccacf8e8477c8a1baf834c09a5
bandreghetti <brunoandreghetti@gmail.com>
2017-07-02 15:51:21 -0300

    Improved plotting routine

--- calcAngles.m
+++ calcAngles.m
@@ -4,21 +4,35 @@ function [theta0, theta1] = calcAngles(x, y)
 	l2 = 80;
 	
 	theta1 = -acosd((x^2+y^2-l1^2-l2^2)/(2*l1*l2));
-	theta0 = atan2d((y*(l1+l2*cosd(theta2))-x*l2*sind(theta2)), (x*(l1+l2*cosd(theta2))+y*l2*sind(theta2)));
+	theta0 = atan2d((y*(l1+l2*cosd(theta1))-x*l2*sind(theta1)), (x*(l1+l2*cosd(theta1))+y*l2*sind(theta1)));
 
 	%theta2 = acosd((x^2+y^2-l1^2-l2^2)/(2*l1*l2));
-	%theta1 = atand((y*(l1+l2*cosd(theta2))-x*l2*sind(theta2))/(x*(l1+l2*cosd(theta2))+y*l2*sind(theta2)));
+	%theta1 = atand((y*(l1+l2*cosd(theta1))-x*l2*sind(theta1))/(x*(l1+l2*cosd(theta1))+y*l2*sind(theta1)));
 	
-	theta0
-	theta1
+	theta0;
+	theta1;
 	
-	x = (l1*cosd(theta0) + l2*cosd(theta0+theta1))
-	y = l1*sind(theta0) + l2*sind(theta0+theta1)
+	x = (l1*cosd(theta0) + l2*cosd(theta0+theta1));
+	y = l1*sind(theta0) + l2*sind(theta0+theta1);
 
-	X = [l1*cosd(theta0); x];
-	Y = [l1*sind(theta0); y];
+	X = [0; l1*cosd(theta0); x];
+	Y = [0; l1*sind(theta0); y];
 	
-	plot(X, Y, 'o')
-	axis([-160, +160, -160, 160])
+	% Plot the work area
+	
+	t0 = [0:1:180];
+	t1 = [-180:1:0];
+	
+	c1x = 80*cosd(t1+180)-80;
+	c1y = 80*sind(t1+180);
+	c2x = 160*cosd(t0);
+	c2y = 160*sind(t0);
+	c3x = 40*cosd(t0);
+	c3y = 40*sind(t0);
+	c4x = [-160:160];
+	c4y = 0;
+	
+	%plot(c1x, c1y, 'k', c2x, c2y, 'k', c3x, c3y, 'k', c4x, c4y, 'k', X, Y, )
+	%axis([-160, +160, -160, 160])
 	
 endfunction
